38c15b72e88a9a697e7b4a0ea7a50391a720921a
machine1104 <machine1104@gmail.com>
2017-01-27 15:31:49 +0100

    prova3

--- app/src/main/java/app/movemate/LoginActivity.java
+++ app/src/main/java/app/movemate/LoginActivity.java
@@ -9,6 +9,6 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //prova
+
     }
 }
