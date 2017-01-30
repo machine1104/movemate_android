fcd19024e4d509d438b0f1947f658550792a0b4f
machine1104 <machine1104@gmail.com>
2017-01-30 13:30:52 +0100

    Aggiunto Autocompletamento Partenza/Destinazione Google

--- app/src/main/java/app/movemate/CreateFragment.java
+++ app/src/main/java/app/movemate/CreateFragment.java
@@ -1,43 +1,72 @@
 package app.movemate;
 
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class CreateFragment extends Fragment {
-    private SeekBar avaible_bar,price_bar;
-    private TextView avaible,price;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+
+public class CreateFragment extends Fragment implements GoogleApiClient.OnConnectionFailedListener,
+        GoogleApiClient.ConnectionCallbacks {
+    private SeekBar avaible_bar, car_price_bar, moto_price_bar;
+    private TextView avaible, car_price, moto_price;
     private RadioGroup vehicles;
+    private static final String LOG_TAG = "MainActivity";
+    private static final int GOOGLE_API_CLIENT_ID = 0;
+    private GoogleApiClient mGoogleApiClient;
+    private PlaceArrayAdapter mPlaceArrayAdapter;
+    private static final LatLngBounds BOUNDS = new LatLngBounds(
+            new LatLng(41.891527, 12.491170), new LatLng(41.891527, 12.491170));
     View v;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        v =  inflater.inflate(R.layout.fragment_create, container, false);
 
-        //Barra posti disponibili
-        avaible_bar = (SeekBar) v.findViewById(R.id.seekBar);
-        avaible = (TextView) v.findViewById(R.id.text2);
-        avaible.setText(avaible_bar.getProgress()+1+"");
+        v = inflater.inflate(R.layout.fragment_create, container, false);
+
+        //-------------------------Barra posti disponibili Car
+        avaible_bar = (SeekBar) v.findViewById(R.id.car_avaible_seekBar);
+        avaible = (TextView) v.findViewById(R.id.car_avaible);
+        avaible.setText(avaible_bar.getProgress() + 1 + "");
         avaible_bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progress = 1;
+
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 progress = i;
-                avaible.setText(progress+1+"");
+                avaible.setText(progress + 1 + "");
 
             }
 
@@ -48,25 +77,25 @@ public class CreateFragment extends Fragment {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                avaible.setText(progress+1+"");
+                avaible.setText(progress + 1 + "");
 
             }
         });
 
-        //Barra prezzo
-        price_bar = (SeekBar)v.findViewById(R.id.seekBar2);
-        price = (TextView)v.findViewById(R.id.price);
-        price.setText("FREE");
-        price_bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        //-------------------------Barra prezzo Car
+        car_price_bar = (SeekBar) v.findViewById(R.id.car_price_seekBar);
+        car_price = (TextView) v.findViewById(R.id.car_price);
+        car_price.setText("FREE");
+        car_price_bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progress = 0;
+
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 progress = i;
-                if (progress == 0){
-                    price.setText("FREE");
-                }
-                else{
-                    price.setText(progress+"€");
+                if (progress == 0) {
+                    car_price.setText("FREE");
+                } else {
+                    car_price.setText(progress + "€");
                 }
 
             }
@@ -78,43 +107,128 @@ public class CreateFragment extends Fragment {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                if (progress == 0){
-                    price.setText("FREE");
-                }
-                else{
-                    price.setText(progress+"€");
+                if (progress == 0) {
+                    car_price.setText("FREE");
+                } else {
+                    car_price.setText(progress + "€");
                 }
             }
         });
 
-        //Visualizzazione prezzo
-        vehicles = (RadioGroup)v.findViewById(R.id.vehicles);
+        //-------------------------Barra prezzo Moto
+        moto_price_bar = (SeekBar) v.findViewById(R.id.moto_price_seekBar);
+        moto_price = (TextView) v.findViewById(R.id.moto_price);
+        moto_price.setText("FREE");
+        moto_price_bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                progress = i;
+                if (progress == 0) {
+                    moto_price.setText("FREE");
+                } else {
+                    moto_price.setText(progress + "€");
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if (progress == 0) {
+                    moto_price.setText("FREE");
+                } else {
+                    moto_price.setText(progress + "€");
+                }
+            }
+        });
+
+
+        //-------------------------Visualizzazione prezzo/disponibilità
+        final RelativeLayout car_price = (RelativeLayout) v.findViewById(R.id.car_price_layout);
+        final RelativeLayout car_avaible = (RelativeLayout) v.findViewById(R.id.car_avaible_layout);
+        final RelativeLayout moto_price = (RelativeLayout) v.findViewById(R.id.moto_price_layout);
+        final LinearLayout public_layout = (LinearLayout) v.findViewById(R.id.public_transport_layout);
+        car_price.setVisibility(View.GONE);
+        car_avaible.setVisibility(View.GONE);
+        moto_price.setVisibility(View.GONE);
+        public_layout.setVisibility(View.GONE);
+
+        vehicles = (RadioGroup) v.findViewById(R.id.vehicles);
         vehicles.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                if (i == R.id.car || i == R.id.moto ){
-                    RelativeLayout ly = (RelativeLayout) v.findViewById(R.id.price_layout);
-                    ly.setVisibility(View.VISIBLE);
-                }else{
-                    RelativeLayout ly = (RelativeLayout) v.findViewById(R.id.price_layout);
-                    ly.setVisibility(View.GONE);
+                if (i == R.id.car) {
+                    car_price.setVisibility(View.VISIBLE);
+                    car_avaible.setVisibility(View.VISIBLE);
+                    moto_price.setVisibility(View.GONE);
+                    public_layout.setVisibility(View.GONE);
+                }
+                if (i == R.id.moto) {
+                    car_price.setVisibility(View.GONE);
+                    car_avaible.setVisibility(View.GONE);
+                    public_layout.setVisibility(View.GONE);
+                    moto_price.setVisibility(View.VISIBLE);
+                }
+                if (i == R.id.public_transport) {
+                    car_price.setVisibility(View.GONE);
+                    car_avaible.setVisibility(View.GONE);
+                    moto_price.setVisibility(View.GONE);
+                    public_layout.setVisibility(View.VISIBLE);
                 }
             }
         });
 
-        //Autocomplete
 
-        String[] locations = new String[] {"paperina","paperone","paperino","paperoga"};
-        AutoCompleteTextView from = (AutoCompleteTextView) v.findViewById(R.id.from);
-        AutoCompleteTextView to = (AutoCompleteTextView) v.findViewById(R.id.to);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                getActivity(),android.R.layout.simple_dropdown_item_1line,locations
-                );
-        from.setAdapter(adapter);
-        to.setAdapter(adapter);
+        //-------------------------Autocomplete
+        final AutoCompleteTextView from = (AutoCompleteTextView) v.findViewById(R.id.from);
+        final AutoCompleteTextView to = (AutoCompleteTextView) v.findViewById(R.id.to);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addApi(Places.GEO_DATA_API)
+                .enableAutoManage((FragmentActivity) getActivity(), GOOGLE_API_CLIENT_ID, this)
+                .addConnectionCallbacks(this)
+                .build();
+
+        mPlaceArrayAdapter = new PlaceArrayAdapter(getActivity(), android.R.layout.simple_list_item_1,
+                BOUNDS, null);
+
+        from.setThreshold(3);
+        from.setOnItemClickListener(mAutocompleteClickListener);
+        from.setAdapter(mPlaceArrayAdapter);
+
+
+        to.setThreshold(3);
+        to.setOnItemClickListener(mAutocompleteClickListener);
+        to.setAdapter(mPlaceArrayAdapter);
 
 
 
+        ImageButton imageButtonFrom = (ImageButton)v.findViewById(R.id.my_location_from);
+        imageButtonFrom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                from.setText("Posizione Attuale");
+
+
+            }
+        });
+        ImageButton imageButtonTo = (ImageButton)v.findViewById(R.id.my_location_to);
+        imageButtonTo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                to.setText("Posizione Attuale");
+
+
+            }
+        });
+
+        //------------------------------------
 
 
 
@@ -123,5 +237,60 @@ public class CreateFragment extends Fragment {
         return v;
     }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mPlaceArrayAdapter.setGoogleApiClient(mGoogleApiClient);
+        Log.i(LOG_TAG, "Google Places API connected.");
 
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mPlaceArrayAdapter.setGoogleApiClient(null);
+        Log.e(LOG_TAG, "Google Places API connection suspended.");
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e(LOG_TAG, "Google Places API connection failed with error code: "
+                + connectionResult.getErrorCode());
+
+        Toast.makeText(getActivity(),"Google Places API connection failed with error code:" +
+                connectionResult.getErrorCode(),Toast.LENGTH_LONG).show();
+    }
+
+    private AdapterView.OnItemClickListener mAutocompleteClickListener
+            = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            InputMethodManager in = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            in.hideSoftInputFromWindow(view.getApplicationWindowToken(), 0);
+        }
+    };
+
+    private ResultCallback<PlaceBuffer> mUpdatePlaceDetailsCallback
+            = new ResultCallback<PlaceBuffer>() {
+        @Override
+        public void onResult(PlaceBuffer places) {
+            if (!places.getStatus().isSuccess()) {
+                Log.e(LOG_TAG, "Place query did not complete. Error: " +
+                        places.getStatus().toString());
+                return;
+            }
+            // Selecting the first object buffer.
+            final Place place = places.get(0);
+            CharSequence attributions = places.getAttributions();
+            /*
+            mNameTextView.setText(Html.fromHtml(place.getName() + ""));
+            mAddressTextView.setText(Html.fromHtml(place.getAddress() + ""));
+            mIdTextView.setText(Html.fromHtml(place.getId() + ""));
+            mPhoneTextView.setText(Html.fromHtml(place.getPhoneNumber() + ""));
+            mWebTextView.setText(place.getWebsiteUri() + "");
+            if (attributions != null) {
+                mAttTextView.setText(Html.fromHtml(attributions.toString()));
+            }*/
+        }
+    };
 }
+
--- app/src/main/java/app/movemate/MainActivity.java
+++ app/src/main/java/app/movemate/MainActivity.java
@@ -1,20 +1,26 @@
 package app.movemate;
 
 
+import android.Manifest;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
     private BottomNavigationView mBottomNav;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,6 +88,11 @@ public class MainActivity extends AppCompatActivity {
         imm.hideSoftInputFromWindow(getWindow().getDecorView().getWindowToken(), 0);
         fragmentTransaction.commit();
     }
+
+
+
+
+
 
 
 
