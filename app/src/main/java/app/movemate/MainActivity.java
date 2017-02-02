f1431554efcb1069d8b8d2e47e8d049f219c3439
machine1104 <machine1104@gmail.com>
2017-02-02 10:52:32 +0100

    Menu Laterale - Inserita Foto Profilo e Aggiustato Menu

--- app/src/main/java/app/movemate/MainActivity.java
+++ app/src/main/java/app/movemate/MainActivity.java
@@ -4,18 +4,37 @@ package app.movemate;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends ActionBarActivity {
     private BottomNavigationView mBottomNav;
+    private ImageView imageView;
 
 
 
@@ -24,6 +43,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //----------------------BOTTOM NAVIGATION BAR
 
         mBottomNav = (BottomNavigationView) findViewById(R.id.navigationView);
         mBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -66,6 +87,37 @@ public class MainActivity extends ActionBarActivity {
                 return true;
             }
         });
+
+        //----------------------DRAWER PROFILE PIC
+        Bundle params = new Bundle();
+        params.putString("fields", "id,email,gender,cover,picture.type(large)");
+        new GraphRequest(AccessToken.getCurrentAccessToken(), "me", params, HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    @Override
+                    public void onCompleted(GraphResponse response) {
+                        if (response != null) {
+                            try {
+                                JSONObject data = response.getJSONObject();
+                                if (data.has("picture")) {
+                                    String profilePicUrl = data.getJSONObject("picture").getJSONObject("data").getString("url");
+                                    NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+                                    View dView =  navigationView.getHeaderView(0);
+                                    imageView = (ImageView) dView.findViewById(R.id.photo);
+                                    new bitMapTask().execute(profilePicUrl);
+
+
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }).executeAsync();
+
+
+
+
+
         View view = mBottomNav.findViewById(R.id.find);
         view.performClick();
 
@@ -89,6 +141,37 @@ public class MainActivity extends ActionBarActivity {
         imm.hideSoftInputFromWindow(getWindow().getDecorView().getWindowToken(), 0);
         fragmentTransaction.commit();
     }
+
+    class bitMapTask extends AsyncTask<String,ImageView, Bitmap> {
+
+        private Exception exception;
+
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(input);
+                return myBitmap;
+
+            } catch (Exception e) {
+                this.exception = e;
+
+                return null;
+            }
+        }
+
+        protected void onPostExecute(Bitmap bit) {
+            imageView.setImageBitmap(bit);
+
+        }
+
+    }
+
 
 
 
