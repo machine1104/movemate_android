4fef150be1843af4537bb1f5ebe39a7eb6e71ae5
machine1104 <machine1104@gmail.com>
2017-04-02 22:21:08 +0200

    Implementato Picasso

--- app/src/main/java/app/movemate/LoginActivity.java
+++ app/src/main/java/app/movemate/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -61,7 +62,7 @@ public class LoginActivity extends Activity {
             }
         });
 
-        ImageButton btn = (ImageButton) findViewById(R.id.btn);
+        Button btn = (Button) findViewById(R.id.btn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
