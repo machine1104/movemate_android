63700b3b03142b086a8141f4a7627a45ed4621d6
machine1104 <machine1104@gmail.com>
2017-04-19 21:12:41 +0200

    Final Submission

---
+++ app/build/generated/source/buildConfig/release/app/movemate/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package app.movemate;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "app.movemate";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 9;
+  public static final String VERSION_NAME = "1.2.6";
+}
